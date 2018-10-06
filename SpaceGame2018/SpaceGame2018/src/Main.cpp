d355008e48929666dd7adb13e4f5a6e65bbee2f0
Thomas Parker <parkert1911@gmail.com>
2018-10-05 23:42:22 -0700

    Got camera working.

--- SpaceGame2018/SpaceGame2018/src/Main.cpp
+++ SpaceGame2018/SpaceGame2018/src/Main.cpp
@@ -6,19 +6,25 @@
 
 
 
-
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include "utils/MiscUtils.hpp"
 
+
 #include "Camera.h"
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow *window);
 
 const char *vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
 "uniform mat4 projection;\n"
+"uniform mat4 view;\n"
 "void main()\n"
 "{\n"
-"   	gl_Position = projection * vec4(aPos, 1.0f);\n"
+"   	gl_Position = projection *view* vec4(aPos, 1.0f);\n"
 "}\0";
 const char *fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
@@ -27,17 +33,28 @@ const char *fragmentShaderSource = "#version 330 core\n"
 "   FragColor = vec4(0.0f, 0.0f, 0.7f, 1.0f);\n"
 "}\n\0";
 
-int main(void)
-{
+//glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+//glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+//glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
+    GLFWwindow* window;
+	bool firstMouse = true;
 	int width = 640;
 	int height = 480;
-	const char* name = "Space Game";
+	float lastX = width / 2.0f;
+	float lastY = height / 2.0f;
+	bool displayCursor = false;
+int main(void)
+{
 
-	// Camera
-	Camera camera;
 	MiscUtils misc;
 
-    GLFWwindow* window;
 
     /* Initialize the library */
     if (!glfwInit())
@@ -48,6 +65,7 @@ int main(void)
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+	const char* name = "Space Game";
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(width, height, name, NULL, NULL);
@@ -66,6 +84,8 @@ int main(void)
 		std::cout << "Glew didnt init!" << std::endl;
 	}
 
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 
 	// build and compile our shader program
@@ -136,13 +156,38 @@ int main(void)
 	glBindVertexArray(0);
 
 
-	camera.Position.x = 40;
-	camera.Position.z = 40;
+	//camera.Position.x = 40;
+	//camera.Position.z = 40;
+
+
+	glm::mat4 view;
+
 
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+
+		if (!displayCursor)
+		{
+			glfwSetCursorPosCallback(window, mouse_callback);
+			// tell GLFW to capture our mouse
+			glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+		}
+		else
+		{
+			glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+			firstMouse = true;
+		}
+		// per-frame time logic
+// --------------------
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+		// input
+		// -----
+		processInput(window);
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
@@ -155,26 +200,73 @@ int main(void)
 // -----------------------------------------------------------------------------------------------------------
 		glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)width / (float)height, 0.1f, 1000.0f);
 
+		glm::mat4 view = camera.GetViewMatrix();
+
 		glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+		glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "view"), 1, GL_FALSE, glm::value_ptr(view));
 
 
 		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
 		glDrawArrays(GL_TRIANGLES, 0, 3);
-		// glBindVertexArray(0); // no need to unbind it every time 
-
-
-		/////////////////
-
 		
         glfwSwapBuffers(window);
 
         /* Poll for and process events */
         glfwPollEvents();
 
-
 		misc.frameCount();
     }
 
     glfwTerminate();
     return 0;
+}
+void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(RIGHT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_O) == GLFW_PRESS)
+		displayCursor = true;
+	if (glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS)
+		displayCursor = false;
+}
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (displayCursor == false)
+	{
+
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+	}
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
 }
